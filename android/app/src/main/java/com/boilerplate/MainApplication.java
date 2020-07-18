eb8e9e5428c3bfc8d92c5d333d2d5ab730b1780f
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-18 13:00:45 +0200

    boilder

---
+++ android/app/src/main/java/com/boilerplate/MainApplication.java
@@ -0,0 +1,55 @@
+package com.boilerplate;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.vonovak.AddCalendarEventPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new AddCalendarEventPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+            new FBSDKPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
